4339afde1225221d7edc21aaa1bd81c2cc4acbdf
Rowen Qin <chinqrw@gmail.com>
2019-03-23 13:25:10 -0400

    fix： model add null

--- docker/project/info/models.py
+++ docker/project/info/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class CourseInfo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    prerequisites = models.CharField(max_length=50)
-    offered = models.CharField(max_length=20)
-    cross_listed = models.CharField(max_length=50)
-    credit_hours = models.IntegerField()
+    prerequisites = models.CharField(max_length=50, null=True, blank=True)
+    offered = models.CharField(max_length=50, null=True, blank=True)
+    cross_listed = models.CharField(max_length=50, null=True, blank=True)
+    credit_hours = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
