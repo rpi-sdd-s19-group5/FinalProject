ceed25bc89e524a64f53fe1e3b93a94138859e02
Rowen Qin <chinqrw@gmail.com>
2019-03-23 13:17:08 -0400

    feat: update models

--- docker/project/info/course_info.py
+++ docker/project/info/course_info.py
@@ -16,7 +16,7 @@ def course_info_crawler(output_json=False):
         desired_capabilities=DesiredCapabilities.CHROME)
     # chrome = webdriver.Chrome()
     courses_info = []
-    for num in range(1, 1):
+    for num in range(1, 2):
         url = "http://catalog.rpi.edu/content.php?catoid=18&catoid=18&navoid=444&filter%5Bitem_type%5D=3&filter" \
               "%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D=" + str(num) + "#acalog_template_course_filter"
         chrome.get(url)
@@ -57,7 +57,7 @@ def course_info_crawler(output_json=False):
                     elif "Credit Hours" in line:
                         course["credit_hours"] = line
                 courses_info.append(course)
-                print(course)
+                print(str(course["title"]))
 
             except TimeoutException as e:
                 print("Elements not found")
