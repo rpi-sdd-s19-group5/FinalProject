758eb7d6da0291a39c1e2a8cbc313de60d5c4c19
Rowen Qin <chinqrw@gmail.com>
2019-03-24 15:34:31 -0400

    add: get method

--- docker/project/polls/urls.py
+++ docker/project/polls/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search_course', views.course, name="search_course"),
+    path('search_course', views.search_course, name="search_course"),
+    path('course/<name_num>',views.course_detail, name="course_detail" ),
+
     path('test', views.test, name="test"),
+    path('test2', views.test2, name="test2"),
 ]
--- docker/project/polls/views.py
+++ docker/project/polls/views.py
@@ -2,15 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
+from info.models import CourseInfo
+
 
 def index(request):
+    print("233")
     return render(request, 'polls/main.html')
 
 
 def search_course(request):
     if request.GET:
         print(request.GET)
-    return render(request, 'polls/search_course.html')
+        dept = request.GET["dept"]
+        search_content = request.GET["search_content"]
+        return render(request, 'polls/search_course.html')
+    else:
+        data = CourseInfo.objects.all()[:5]
+        return render(request, 'polls/search_course.html')
 
 
 def test(request):
@@ -19,3 +27,7 @@ def test(request):
 
 def test2(request):
     return render(request, 'polls/course.html')
+
+
+def course_detail(request):
+    return None
