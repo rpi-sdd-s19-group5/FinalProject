f462d4fda3c61f748164a98408e74daa36afd426
huangmj7 <huangm7@rpi.edu>
2019-04-14 16:34:04 -0400

    add notations

--- docker/project/info/scripts/prof_info.py
+++ docker/project/info/scripts/prof_info.py
@@ -5,34 +5,40 @@ import django
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 
+#Setting up environment
 sys.path.append("/src")
 os.environ['DJANGO_SETTINGS_MODULE'] = 'DjangoTest.settings'
 if 'setup' in dir(django):
     django.setup()
 
-
+#Function returns the department and professor's webpages in one time
+#Case 1: get lists of department
+#Case 2: get lists of teacher
 def get_url(driver, case):
     ans = []
     size_need = len("https://faculty.rpi.edu/departments")
-    link = driver.find_elements_by_tag_name("a")
+    link = driver.find_elements_by_tag_name("a") #get all urls in one time
     for n in link:
         try:
             try1 = n.get_attribute('href')
             if case == 1:
+                #Assert the urls is correct
                 if ("https://faculty.rpi.edu/departments" in try1 and len(
                         try1) > size_need and "main-content" not in try1):
                     # print try1
                     ans.append(try1)
             if case == 2:
+                #Assert the urls is correct
                 if "https://faculty.rpi.edu/" in try1 and "-" in try1 and "main-content" not in try1:
                     # print try1
                     ans.append(try1)
         except TypeError as e:
-            print("ERROR:None")
+            #Fail to get the webpage
+            print("ERROR: get_url unable to get attribute from {} ".format(n))
     driver.quit()
     return ans
 
-
+#Function returns a webdriver object
 def get_driver(url):
     driver = webdriver.Remote(
         command_executor='http://selenium:4444/wd/hub',
@@ -40,7 +46,7 @@ def get_driver(url):
     driver.get(url)
     return driver
 
-
+#Funtion returns string of a given web page title
 def get_name(driver):
     name = []
     try:
@@ -48,10 +54,10 @@ def get_name(driver):
         name.append(n.text)
         # print(name.text)
     except:
-        print("?")
+        print("get_name fail to get title text from {}".format(dirver))
     return name
 
-
+#Function returns all the text in input web page
 def get_page(driver):
     ans = []
     im_word = driver.find_elements_by_class_name("view-content")
@@ -60,10 +66,10 @@ def get_page(driver):
             lang = im.text
             ans.append(lang.text)
         except:
-            print("failed")
+            print("get_page fail to get title text from {}".format(dirver))
     return ans
 
-
+#Function returns the image address from given image
 def get_image(driver):
     ans = []
     image = driver.find_elements_by_tag_name("img")
@@ -73,10 +79,10 @@ def get_image(driver):
         try:
             if "https://faculty.rpi.edu/" in try2: ans.append(try2)
         except:
-            print("NO image")
+            print("get_image fail to get image address from {}".format(dirver))
     return ans
 
-
+#Function returns text line based input rules
 def extract(line, re):
     ans = []
     if re != "":
@@ -84,26 +90,26 @@ def extract(line, re):
             try:
                 if re in line: ans.append(line.text)
             except:
-                print("F")
+                print("extract unable to get text from line")
     else:
         for _ in line:
             try:
                 ans.append(line.text)
             except:
-                print("F")
+                print("extract unable to get text from line")
     return ans
 
-
+#Function returns text line from list of selector
 def line(selector):
     ans = []
     for s in selector:
         try:
             ans.append(s.text)
         except:
-            print("No")
+            print("line unable to get test from input selector")
     return ans
 
-
+#Function returns urls get from selector
 def address(selector):
     ans = []
     for s in selector:
@@ -111,10 +117,10 @@ def address(selector):
             x = s.get_attribute('href')
             ans.append(x)
         except:
-            print("No")
+            print("address unable to get address from input selector")
     return ans
 
-
+#Wrapper function that collect all information from a single prfessor page
 def get_people(url):
     try:
         driver = get_driver(url)
@@ -180,7 +186,7 @@ def get_people(url):
     driver.quit()
     return page
 
-
+#Wrapper function that for crawling the whole RPI INFO page for professor information 
 def crawler_wrapper():
     name_list = []  # <-- list of all names
 
@@ -205,7 +211,7 @@ def crawler_wrapper():
 
     return people_list
 
-
+#Start the function
 if __name__ == "__main__":
 
     json_list = crawler_wrapper()
