72241599da62e2f3a254ab6a82095a24641bb257
huangmj7 <huangm7@rpi.edu>
2019-04-25 15:02:22 -0400

    add frontend test

--- docker/project/info/tests.py
+++ docker/project/info/tests.py
@@ -2,6 +2,15 @@ from django.test import TestCase
 
 from info.models import CourseInfo, ProfInfo
 
+import os
+import sys
+import time
+
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class SimpleTestCase(TestCase):
     # this test case tests searching in a specific department with a keyword
@@ -216,4 +225,174 @@ class SimpleTestCase(TestCase):
         # there are 3 results in total
         self.assertEqual(len(search_result), 3)
         ProfInfo.objects.all().delete()
+
+sys.path.append("/src")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'DjangoTest.settings'
+def get_driver(url):
+    driver = webdriver.Remote(command_executor='http://selenium:4444/wd/hub',
+                desired_capabilities=DesiredCapabilities.CHROME)
+    driver.get(url)
+    return driver
+
+#Three basic operation#
+#-------------------------------------------------------------------------------------#
+def enter_(web,loc,data):
+    
+    #input_element = web.find_by_css_selector(loc)
+    input_element =  WebDriverWait(web, 1).until(EC.presence_of_element_located((By.CSS_SELECTOR, loc)))
+    input_element.send_keys(data)
+    
+def click_(web,loc):
+    
+    button_element = web.find_element_by_css_selector(loc)#WebDriverWait(web, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, loc)))
+    button_element.click()
+        
+def select_(web,data):
+    
+    for i in range(1,42):
+        loc = "select.ng-valid > option:nth-child({})".format(i)
+        bar = web.find_element_by_css_selector(loc)
+        if(bar.text == data):
+            bar.click()
+            return
+    bar = web.find_element_by_css_selector("select.ng-valid > option:nth-child(1)")
+    bar.click()
+
+#-------------------------------------------------------------------------------------#
+
+class FrontendTestCase(TestCase):
+    
+   def test_PushButton(self):
+    
+        web = get_driver("http://djangosdd:8080/") #preC
+        #print(web.current_url,": ",web.title)
+        #x = web.find_element_by_xpath("/html/body")
+        #print(x.text)
+        
+        loc = dict()
+        loc["search-button"] = ".btn"
+        loc["nav-bar"] = ".navbar > a:nth-child(1)"
+        loc["input-bar"] = "input.ng-pristine"
+        loc["course-button"] = "li.nav-item:nth-child(1) > a:nth-child(1)"
+        loc["faculty-button"] = "li.nav-item:nth-child(2) > a:nth-child(1)"
+        
+        r0 = dict()
+        r0["title"] = "RPI Advanced Academic Catalog"
+        r0["url"] = "http://djangosdd:8080/"
+        
+        r1 = dict()
+        r1["title"] = "RPI Advanced Academic Catalog"
+        r1["url"] ="http://djangosdd:8080/search_course?dept=string%3AAll+Departments&search_content="
+
+        r2 = dict()
+        r2["title"] = "RPI Advanced Academic Catalog - Prof"
+        r2["url"] ="http://djangosdd:8080/prof"
+
+        #Sequences of action
+
+        #check search button
+        click_(web,loc["search-button"])
+        self.assertEqual(r1["title"],web.title)
+        self.assertEqual(r1["url"],web.current_url)
+
+
+        #check course button
+        click_(web,loc["course-button"])
+        self.assertEqual(r0["title"],web.title)
+        self.assertEqual(r0["url"],web.current_url)
+
+        #check faculty button
+        click_(web,loc["faculty-button"])
+        self.assertEqual(r2["title"],web.title)
+        self.assertEqual(r2["url"],web.current_url)
+
+        #check navgation button 
+        click_(web,loc["nav-bar"])
+        self.assertEqual(r0["title"],web.title)
+        self.assertEqual(r0["url"],web.current_url)
+        print("Button test passed") 
+        web.close()
+        
+   def test_Pagination(self):
+        
+        loc = dict()
+        loc["next"] = "a.active:nth-child(6)"
+        loc["prev"] = "a.active:nth-child(3)"
+        loc["first"] = "li.page-item:nth-child(2) > a:nth-child(1)"
+        loc["last"] = "a.active:nth-child(7)"
+        loc["num"] = "li.active:nth-child(5) > a:nth-child(1)"
+        
+        r0 = dict()
+        r0["title"] = "RPI Advanced Academic Catalog"
+        r0["url"] ="http://djangosdd:8080/search_course?dept=string%3AAll+Departments&search_content="
+
+        r1 = dict()
+        r1["title"] = "RPI Advanced Academic Catalog"
+        r1["url"] ="http://djangosdd:8080/search_course?page=1&dept=string%3AAll+Departments&search_content="
+
+        r2 = dict()
+        r2["title"] = "RPI Advanced Academic Catalog"
+        r2["url"] ="http://djangosdd:8080/search_course?page=2&dept=string%3AAll+Departments&search_content="
+        
+        
+        r3 = dict()
+        r3["title"] = "RPI Advanced Academic Catalog"
+        r3["url"] ="http://djangosdd:8080/search_course?page=3&dept=string%3AAll+Departments&search_content="
+        
+        r4 = dict()
+        r4["title"] = "RPI Advanced Academic Catalog"
+        r4["url"] ="http://djangosdd:8080/search_course?page=4&dept=string%3AAll+Departments&search_content="
+
+
+        CourseInfo.objects.all().delete()
+
+        #generate data needs
+        for i in range(35):
+            
+            CourseInfo.objects.update_or_create(
+                    title="Test{}".format(i), dept="ECSE", course_code="TEST 0001", description="This is {}th test abracadabra".format(i),prerequisites="N/A",
+            offered="Never", cross_listed="Delete me", credit_hours=0
+            )
+        
+        search_result = CourseInfo.search_course_tool("", "Test1")
+        print(len(search_result))
+        search_result = CourseInfo.search_course_tool("", "ALL")
+        print(len(search_result))
+
+        #PreC
+        time.sleep(10)
+        web = get_driver("http://djangosdd:8080/search_course?dept=string%3AAll+Departments&search_content=")
+
+        #Sequence of action
+        
+        #page 2
+
+        elements = WebDriverWait(web, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "body")))
+        #print(elements.text)
+        click_(web,loc["num"])
+        self.assertEqual(web.title,r2["title"])
+        self.assertEqual(web.current_url,r2["url"])
+
+        #prev
+        click_(web,loc["prev"])
+        self.assertEqual(web.title,r1["title"])
+        self.assertEqual(web.current_url,r1["url"])
+
+        #next
+        click_(web,loc["next"])
+        self.assertEqual(web.title,r2["title"])
+        self.assertEqual(web.current_url,r2["url"])
+
+        #first
+        click_(web,loc["first"])
+        self.assertEqual(web.title,r1["title"])
+        self.assertEqual(web.current_url,r1["url"])
+
+        #last
+        click_(web,loc["last"])
+        self.assertEqual(web.title,r4["title"])
+        self.assertEqual(web.current_url,r4["url"])
+
+        CourseInfo.objects.all().delete()
+        web.close()
 
